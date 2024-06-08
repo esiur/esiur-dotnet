2576bfdc059f011025f308d7211c45483e25059b
Ahmed Zamil <ahmed@esiur.com>
2024-06-08 12:56:39 +0300

    demo

---
+++ Esiur.Examples.StandaloneWebServerDemo/Demo.cs
@@ -0,0 +1,18 @@
+﻿using Esiur.Resource;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esiur.Examples.StandaloneWebServerDemo
+{
+    [Export]
+    [Resource]
+    public partial class Demo
+    {
+        [Export] int age { get; set; }
+        [Export] string name { get; set;}
+        [Export] 
+    }
+}
---
+++ Esiur.Examples.StandaloneWebServerDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using Esiur.Net.IIP;
+using Esiur.Resource;
+using Esiur.Stores;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        // Create a store to keep objects.
+        var system = await Warehouse.Put("sys", new MemoryStore());
+        // Create a distibuted server
+        var esiurServer = await Warehouse.Put("sys/server", new DistributedServer());
+        // Add your object to the store
+        var service = await Warehouse.Put("sys/demo", new EsiurGreeter());
+
+
+        // Start your server
+        await Warehouse.Open();
+
+    }
+}
