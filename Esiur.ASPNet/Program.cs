acfb6af27a287b7eed5979d6bc74883c63a0d23b
ahmed <ahmed@esiur.com>
2025-06-30 17:14:34 +0300

    AspNet

--- Esiur.ASPNet/Program.cs
+++ Esiur.ASPNet/Program.cs
@@ -40,10 +40,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+//builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
 
 builder.WebHost.UseUrls("http://localhost:8080");
 
