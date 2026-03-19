93e07859418382c87f1a1013986b6ecfb85343d3
ahmed <ahmed@esiur.com>
2026-03-19 19:33:06 +0300

    Annotations

---
+++ Tests/Annotations/Agent.cs
@@ -0,0 +1,25 @@
+﻿using Esiur.Core;
+using Esiur.Data;
+using Esiur.Misc;
+using Esiur.Protocol;
+using Esiur.Resource;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Esiur.Tests.Annotations;
+
+
+[Resource]
+public partial class Agent;
+
---
+++ Tests/Annotations/Program.cs
@@ -0,0 +1,19 @@
+﻿// The endpoint for LM Studio's local server
+using OpenAI;
+using OpenAI.Chat;
+
+using System.Data;
+
+var endpoint = "http://localhost:1234/v1";
+var client = new OpenAIClient(new OpenAIClientOptions()
+{
+    Endpoint = new Uri("http://localhost:1234/v1")
+});
+
+var chat = client.GetChatClient("local-model");
+
+var response = await chat.CompleteAsync(
+    "Explain what this function does"
+);
+
+Console.WriteLine(response.Value.Content[0].Text);
