e5efc8d91971315d7ca433bbe81a20b4bafc733f
Ahmed Zamil <ahmed@esiur.com>
2026-03-19 19:52:04 +0300

    a

--- Tests/Annotations/Program.cs
+++ Tests/Annotations/Program.cs
@@ -1,19 +1,27 @@
 ﻿// The endpoint for LM Studio's local server
 using OpenAI;
 using OpenAI.Chat;
-
+using System.ClientModel;
 using System.Data;
 
 var endpoint = "http://localhost:1234/v1";
-var client = new OpenAIClient(new OpenAIClientOptions()
-{
-    Endpoint = new Uri("http://localhost:1234/v1")
-});
+var credential = new ApiKeyCredential("lm-studio");
 
-var chat = client.GetChatClient("local-model");
+var client = new OpenAIClient(credential, new OpenAIClientOptions() { Endpoint = new Uri(endpoint) });
 
-var response = await chat.CompleteAsync(
-    "Explain what this function does"
+var chat = client.GetChatClient("microsoft/phi-4");
+
+//List<ChatMessage> messages = new List<ChatMessage>
+//{
+//    new SystemChatMessage("You are a helpful assistant that only speaks in rhymes."),
+//    new UserChatMessage("What is the capital of France?")
+//};
+
+//// Send the entire conversation history
+//ChatCompletion completion = chat.CompleteChat(messages);
+
+var response = await chat.CompleteChatAsync(
+    "Explain what Pi means"
 );
 
 Console.WriteLine(response.Value.Content[0].Text);
